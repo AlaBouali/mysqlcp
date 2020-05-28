71d3e81421f1f5242deeb39c687ca4fbf66915c5
AlaBouali <trap.leader.123@gmail.com>
2020-05-28 23:39:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flask_mysqlcp",
-    version="1.1.0",
+    name="mysqlcp",
+    version="1.0.0",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
-    description="Flask extension to create connection pools with pymysql2",
+    description="Simple, secure and advanced module to perform safe MySQL queries",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AlaBouali/flask_mysqlcp",
+    url="https://github.com/AlaBouali/mysqlcp",
     python_requires=">=2.7",
-    install_requires=['pymysql2'],
-    packages=["flask_mysqlcp"],
+    install_requires=['pymysql'],
+    packages=["mysqlcp"],
     license="MIT License",
+    entry_points={
+       'console_scripts': [
+           'xmysql = mysqlcp.__main__:run',
+       ],
+    },
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License ",
